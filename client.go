d4bdd95003e2cdbbe7153334ae18abad781101b3
hamzaanis <hamzaanis9514@gmail.com>
2019-02-02 15:11:01 +0500

    Added flags for ip and port of server

--- client.go
+++ client.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 )
 
 func main() {
-	conn, err := net.Dial("udp", "127.0.0.1:1234")
+	var port = flag.Int("port", 1234, "port of the udp socket on server")
+	var ip = flag.String("ip", "127.0.0.1", "ip of the server")
+	flag.Parse()
+	connectionString := fmt.Sprintf("%s:%d", *ip, *port)
+	conn, err := net.Dial("udp", connectionString)
 	if err != nil {
 		fmt.Printf("error: %v", err)
 		return
--- udpserver.go
+++ udpserver.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net"
 )
 
@@ -11,15 +13,20 @@ func writeStdOut(msg []byte, addr *net.UDPAddr) {
 
 func main() {
 	buffer := make([]byte, 2048)
+	var port = flag.Int("port", 1234, "the port on which the udp socket will open")
+	var ip = flag.String("ip", "127.0.0.1", "the host addrress for the udp socket")
+
+	flag.Parse()
 	addr := net.UDPAddr{
-		Port: 1234,
-		IP:   net.ParseIP("127.0.0.1"),
+		Port: *port,
+		IP:   net.ParseIP(*ip),
 	}
 	ser, err := net.ListenUDP("udp", &addr)
 	if err != nil {
 		fmt.Printf("error: %v\n", err)
 		return
 	}
+	log.Printf("Udp Socket running on Ip:{%s}, Port:{%v}\n", *ip, *port)
 	for {
 		n, addr, err := ser.ReadFromUDP(buffer)
 		if err != nil {
