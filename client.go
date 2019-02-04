5fe5a141ebc5bbef9c091a633314f12b68ee203f
hamzaanis <hamzaanis9514@gmail.com>
2019-02-05 04:01:01 +0500

    Changing client from flags to arg parse

--- client.go
+++ client.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"strconv"
 
 	"github.com/tarm/serial"
 )
@@ -15,17 +16,36 @@ func sendJson(buffer []byte, conn *net.Conn) {
 	log.Printf("from port: %v\n", string(buffer))
 }
 func main() {
-	var port = flag.Int("port", 1234, "port of the udp socket on server")
-	var ip = flag.String("ip", "127.0.0.1", "ip of the server")
-	var name = flag.String("name", "Serial", "the name of the serial port")
-	var baud = flag.Int("baud", 8000, "the baud rate of the serial port")
+	var ip string
+	var port int
+	var name string
+	var baud int
 
-	flag.Parse()
+	if len(os.Args) < 5 {
+		ip = "127.0.0.1"
+		port = 1234
+		name = "name"
+		baud = 8000
+	} else {
+		ip = os.Args[1]
+		var err error
+		port, err = strconv.Atoi(os.Args[2])
+		if err != nil {
+			log.Println(err)
+			os.Exit(2)
+		}
+		name = os.Args[3]
+		baud, err = strconv.Atoi(os.Args[4])
+		if err != nil {
+			log.Println(err)
+			os.Exit(2)
+		}
+	}
 
 	// opening serial port
 	c := &serial.Config{
-		Name: *name,
-		Baud: *baud,
+		Name: name,
+		Baud: baud,
 	}
 	s, err := serial.OpenPort(c)
 	if err != nil {
@@ -33,7 +53,7 @@ func main() {
 	}
 
 	// opening udp socket
-	connectionString := fmt.Sprintf("%s:%d", *ip, *port)
+	connectionString := fmt.Sprintf("%s:%d", ip, port)
 	conn, err := net.Dial("udp", connectionString)
 	if err != nil {
 		log.Printf("error: %v", err)
